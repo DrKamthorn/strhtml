93c325cb9cd63c41f4cafd107a5dcbb30ade220c
DrKamthorn <132547814+DrKamthorn@users.noreply.github.com>
2023-05-24 12:01:21 +0700

    Update strhtml.py

--- strhtml.py
+++ strhtml.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
-components.iframe("https://drkamthorn-whisperai.streamlit.app/", width= None, height=600, scrolling=True)
+components.iframe("https://drkamthorn-whisperai.streamlit.app/", width= 1500, height=600, scrolling=True)
 
 
 
