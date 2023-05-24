e1fc4dc1000a566d899bac42975c25925a0d00cb
DrKamthorn <132547814+DrKamthorn@users.noreply.github.com>
2023-05-24 12:05:11 +0700

    Update strhtml.py

--- strhtml.py
+++ strhtml.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
-components.iframe("http://sigj.atwebpages.com/doc/index.php", width= 1500, height=600, scrolling=True)
+components.iframe("https://drkamthorn-check-up.streamlit.app/", width= 1500, height=600, scrolling=True)
 
 
 
